986b46e8cd327086ab479bf6addaa91b0ca3ab67
streanger <pslisz@o2.pl>
2024-01-11 20:03:50 +0100

    map packages to dict

--- get_rid_of_windows_bloatware/python_powershell.py
+++ get_rid_of_windows_bloatware/python_powershell.py
@@ -1,7 +1,24 @@
+import csv
 import ctypes
+import json
 import os
-import sys
 import subprocess
+import sys
+import time
+from pathlib import Path
+
+from rich import print
+
+
+def now():
+    return time.strftime('%Y%m%d%H%M%S')
+
+
+def write_json(filename, data):
+    """write to json file"""
+    with open(filename, 'w', encoding='utf-8') as fp:
+        json.dump(data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+    return True
 
 
 def rerun_as_admin():
@@ -20,11 +37,29 @@ def run(cmd):
 
 
 if __name__ == '__main__':
+    os.chdir(str(Path(__file__).parent))
     rerun_as_admin()
-    command = "Get-AppxPackage -AllUsers | Select Name, PackageFullName"
+
+    # **** run command ****
+    # command = "Get-AppxPackage -AllUsers | Select Name, PackageFullName"
+    # command = "Get-AppxPackage -AllUsers | Select Name, PackageFullName | Export-Csv -Path .\output.csv -NoTypeInformation"
+    command = "Get-AppxPackage -AllUsers | Select Name, PackageFullName | ConvertTo-Csv -NoTypeInformation"
     response = run(command)
     stdout = response.stdout.decode('852')
     stderr = response.stderr.decode('852')
-    output = stdout or stderr
-    print(output)
-    
+    if not stdout:
+        print(stderr)
+        sys.exit(1)
+
+    # **** parse output ****
+    csv_reader = csv.DictReader(stdout.splitlines())
+    packages = {row['Name']: row['PackageFullName'] for row in csv_reader}
+    print(packages)
+
+    # **** save to json file ****
+    directory = Path('appxpackages')
+    directory.mkdir(exist_ok=True)
+    out = f'appxpackages-{now()}.csv'
+    path = directory.joinpath(out)
+    write_json(path, packages)
+    print(f'[*] data saved to: {path}')
